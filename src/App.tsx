436252672fbcfbfc3a937020de8655673c142bf9
devfranclinjames <devfranclinjames@gmail.com>
2024-11-06 19:41:42 +0800

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <h1>rhea lim</h1>
       </div>
       <h1>Vite + React</h1>
     </>
