62814d6d38777afa4bfaaa720f28bdaca92a5a8a
devfranclinjames <devfranclinjames@gmail.com>
2024-11-06 21:38:23 +0800

    reactrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      test
     </>
   )
 }
